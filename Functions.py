2688471fd21ccf899196ec5fafba6ab94ceb0705
YaBoyNary <46732148+YaBoyNary@users.noreply.github.com>
2020-01-26 22:58:38 +1100

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -2,36 +2,32 @@
 implementation of the cost function and its partial derivatives
 """
 
-from typing import List
+def partial_theta0(theta0, theta1, data):
+    sum_component, n = 0, len(data.index)
+    y = data['y']
+    x = data['x']
 
-
-def partial_theta1(theta1, theta2, data: List):
-    sum_component, n = 0, len(data)
-
-    for x in range(len(data)):
-        y = data[x]
-        sum_component += theta1 + theta2 * x - y
-
+    for i in range(len(data.index)):
+        sum_component += theta0 + theta1 * x[i] - y[i]
     return sum_component / n
 
 
-def partial_theta2(theta1, theta2, data: List):
-    sum_component, n = 0, len(data)
+def partial_theta1(theta0, theta1, data):
+    sum_component, n = 0, len(data.index)
+    y = data['y']
+    x = data['x']
 
-    for x in range(len(data)):
-        y = data[x]
-
-        sum_component += (theta1 + theta2*x - y)*x
-
+    for i in range(len(data.index)):
+        sum_component += (theta0 + theta1 * x[i] - y[i]) * x[i]
     return sum_component / n
 
 
-def cost_func(theta1, theta2, data: List):
-    sum_component, n = 0, len(data)
+def cost_func(theta0, theta1, data):
+    sum_component, n = 0, len(data.index)
+    y = data['y']
+    x = data['x']
 
-    for x in range(len(data)):
-        y = data[x]
-
-        sum_component += (theta1 + theta2*x - y)**2
+    for i in range(len(data.index)):
+        sum_component += (theta0 + theta1 * x[i] - y[i])**2
 
     return sum_component / 2*n
